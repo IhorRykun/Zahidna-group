1a2778ff1edf9b5f0a734ef71575d5515de7e542
Ihor <ihorrykun@gmail.com>
2023-01-02 16:07:26 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-import './js/swiper';
+const button = document.querySelector('.service__button');
+
+button.addEventListener('click', openClickButton);
+
+function openClickButton() {
+  window.open('./service.html');
+}
